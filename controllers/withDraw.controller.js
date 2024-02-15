2a76c0a171f361416ad477032cc65f358ae092b2
Survesh Pandit <157343138+SuryaSuru@users.noreply.github.com>
2024-02-15 18:52:43 +0530

    feat:done the withdraw update

--- controllers/withDraw.controller.js
+++ controllers/withDraw.controller.js
@@ -2,21 +2,39 @@ import WithDrawModel from '../models/withDraw.model.js';
 
 export async function viewWithdrawRequests(req, res) {
   try {
-    const adminId = req.params.adminId;
-    console.log("adminId--->", adminId);
+    const userId = req.params.userId;
+    console.log("userId--->", userId);
 
-    const admin = await WithDrawModel.findOne({ adminId: adminId });
-    if (!admin) {
-      console.error(`Admin with adminId ${adminId} not found`);
-      return res.status(404).json({ message: "Admin not found" });
+    const withdraws = await WithDrawModel.find({ userId: userId });
+    console.log("admin--->", withdraws);
+    if (!withdraws) {
+      // console.error(`Admin with adminId ${adminId} not found`);
+      return res.status(404).json({ message: "Something Went Wrong" });
     }
 
-    console.log("admin--->", admin);
-
-    const withdrawals = await WithDrawModel.find({ adminId: adminId });
-    res.status(200).json({ withdrawals });
+    // const withdrawals = await WithDrawModel.find({ adminId: adminId });
+    res.status(200).json({ withdraws });
   } catch (error) {
     console.error("Error in viewWithdrawRequests:", error);
+    res.status(500).json({ message: "Something went wrong", error: error.message });
+  }
+}
+
+// Define the route and method to handle GET requests for all withdrawal requests
+export async function getAllWithdrawRequests(req, res) {
+  try {
+    // Retrieve all withdrawal requests from the database
+    const withdraws = await WithDrawModel.find();
+
+    // Check if there are any withdrawal requests
+    if (withdraws.length === 0) {
+      return res.status(404).json({ message: "No withdrawal requests found" });
+    }
+
+    // If withdrawal requests are found, return them in the response
+    res.status(200).json({ withdraws });
+  } catch (error) {
+    console.error("Error in getAllWithdrawRequests:", error);
     res.status(500).json({ message: "Something went wrong", error: error.message });
   }
 }
--- routes/withDraw.route.js
+++ routes/withDraw.route.js
@@ -3,7 +3,10 @@ import * as withDrawController from '../controllers/withDraw.controller.js';
 
 const router = express.Router();
 
+// userId requests
+router.get('/withdrawal-requests/:userId', withDrawController.viewWithdrawRequests);
+
 // all requests
-router.get('/:adminId/withdrawal-requests', withDrawController.viewWithdrawRequests);
+router.get('/withdraws', withDrawController.getAllWithdrawRequests);
 
 export default router;
